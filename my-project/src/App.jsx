b7654b5825724c75cc3634f79bddae003fdcc764
Vitor Burnier <burnvitor2@gmail.com>
2025-09-05 12:56:13 -0300

    home

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,43 @@
-
 import './App.css'
+import Login from './pages/Login'
+import { Routes, Route } from 'react-router-dom';
+import { Link } from "react-router";
+
 
 function App() {
 
   return (
-    <h1 className='text-amber-600'>OLa mundo</h1>
+    //main
+    <div className='w-screen h-screen flex justify-center '>
+
+      <header className="flex justify-between items-center h-15 container">
+        <p className="font-bold text-3xl">Bem-Vindo</p>
+
+        <div className='flex w-50 justify-between' >
+
+          <Link to="/Login" className=" flex justify-center items-center rounded">
+            <p className='text-f'>Sign in</p>
+          </Link>
+
+          {/*
+          *
+          precisa mudar a rota
+          *
+          */}
+          <Link to="/Login" className=" flex justify-center items-center w-20 h-10 rounded buttons">
+            <h1 className='text-f'>Sign up</h1>
+          </Link>
+          
+        </div>
+        
+      </header>
+
+    
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+
+    </div>
   )
 }
 
